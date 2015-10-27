f9dd2f7721fe1e3a32367c8696be17191e28dddb
anashel <anas.helalouch@gmail.com>
2015-10-27 11:07:33 +0100

    test

--- Flattitude/app/src/main/java/com/aseupc/flattitude/LoginActivity.java
+++ Flattitude/app/src/main/java/com/aseupc/flattitude/LoginActivity.java
@@ -30,8 +30,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.aseupc.databasefacade.UserFacade;
-
 import java.util.ArrayList;
 import java.util.List;
 

95475cbc1524ea82d792bcf68b78400a4753a632
Jordi Coll Caballero <u1906230@campus.udg.edu>
2015-10-30 18:52:17 +0100

    Interaction with the map by means of a button

--- Flattitude/app/src/main/java/com/aseupc/flattitude/LocateObjectsActivity.java
+++ Flattitude/app/src/main/java/com/aseupc/flattitude/LocateObjectsActivity.java
@@ -1,13 +1,23 @@
 package com.aseupc.flattitude;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class LocateObjectsActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class LocateObjectsActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    final OnMapReadyCallback t = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,28 @@ public class LocateObjectsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MapFragment mapFragment = (MapFragment) getFragmentManager()
+                        .findFragmentById(R.id.map);
+                mapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap googleMap) {
+                        googleMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(0, 0))
+                                .title("Marker"));
+                    }
+                });
+            }
+        });
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +57,11 @@ public class LocateObjectsActivity extends AppCompatActivity {
             }
         });*/
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
 
 }

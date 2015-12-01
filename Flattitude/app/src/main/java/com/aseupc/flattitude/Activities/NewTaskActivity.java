d99b7f232d7e9efa05299084bb1ac6f66978ca1b
Jordi Coll Caballero <u1906230@campus.udg.edu>
2015-12-01 17:41:36 +0100

    date picker and time picker

--- Flattitude/app/src/main/java/com/aseupc/flattitude/Activities/NewTaskActivity.java
+++ Flattitude/app/src/main/java/com/aseupc/flattitude/Activities/NewTaskActivity.java
@@ -1,15 +1,35 @@
 package com.aseupc.flattitude.Activities;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.aseupc.flattitude.R;
 
-public class NewTaskActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class NewTaskActivity extends AppCompatActivity
+        implements TimePickerDialog.OnTimeSetListener,
+        DatePickerDialog.OnDateSetListener
+{
+    private EditText date;
+    private EditText time;
+    private Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +37,91 @@ public class NewTaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_task);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        date = (EditText) findViewById(R.id.date);
+        final DatePickerDialog.OnDateSetListener listenerDate = this;
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerFragment newFragment = new DatePickerFragment(listenerDate);
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+            }
+        });
+        time = (EditText) findViewById(R.id.time);
+        final TimePickerDialog.OnTimeSetListener listenerTime = this;
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerFragment newFragment = new TimePickerFragment(listenerTime);
+                newFragment.show(getSupportFragmentManager(), "timePicker");
+            }
+        });
+
+        calendar = Calendar.getInstance();
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
+        calendar.set(Calendar.MINUTE, minute);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        time.setText(dateFormat.format(calendar.getTime()));
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        calendar.set(Calendar.DAY_OF_MONTH,day);
+        calendar.set(Calendar.MONTH,month);
+        calendar.set(Calendar.YEAR, year);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        time.setText(dateFormat.format(calendar.getTime()));
+
+
+    }
+
+    public static class TimePickerFragment extends DialogFragment {
+        TimePickerDialog.OnTimeSetListener listener;
+
+        public TimePickerFragment(TimePickerDialog.OnTimeSetListener listener){
+            super();
+            this.listener = listener;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), listener, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+    }
+
+    public static class DatePickerFragment extends DialogFragment {
+
+        DatePickerDialog.OnDateSetListener listener;
+
+        public DatePickerFragment(DatePickerDialog.OnDateSetListener listener){
+            super();
+            this.listener = listener;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), listener, year, month, day);
+        }
 
     }
 

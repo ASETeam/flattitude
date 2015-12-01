928252c2f72f49aa0981420a9f75f183cc3075d6
Fara Nabilla <fara.nabilla93@gmail.com>
2015-12-01 16:22:32 +0100

    another UI editing

--- Flattitude/app/src/main/java/com/aseupc/flattitude/Activities/RegisteringActivity.java
+++ Flattitude/app/src/main/java/com/aseupc/flattitude/Activities/RegisteringActivity.java
@@ -40,6 +40,8 @@ public class RegisteringActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registering);
+
+        //customize font for each textview, edit text, etc.
         Typeface customFontButton = Typeface.createFromAsset(getAssets(),"Montserrat-Regular.ttf");
         Typeface customFont = Typeface.createFromAsset(getAssets(),"Quicksand_Bold.otf");
         TextView email_address_label = (TextView)findViewById(R.id.email_address_label);

3d88ebcde0b0877ae76e5c7368f608d5aa29b6fd
anashel <anas.helalouch@gmail.com>
2015-12-10 10:40:31 +0100

    action bar items

--- Flattitude/app/src/main/java/com/aseupc/flattitude/Activities/CreateFlat.java
+++ Flattitude/app/src/main/java/com/aseupc/flattitude/Activities/CreateFlat.java
@@ -72,7 +72,7 @@ public class CreateFlat extends AppCompatActivity {
         mAddress = (EditText) findViewById(R.id.address);
         mCity = (EditText) findViewById(R.id.city);
         mPostalCode = (EditText) findViewById(R.id.postal_code);
-        mCountry = (EditText) findViewById(R.id.postcode);
+        mCountry = (EditText) findViewById(R.id.country);
         mIban = (EditText) findViewById(R.id.iban);
 
         final Button mCreateFlat = (Button) findViewById(R.id.create_button);

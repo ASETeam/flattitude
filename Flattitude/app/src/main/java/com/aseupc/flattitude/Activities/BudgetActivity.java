93a53be02afef5fe4d50fb4756798f7f12a9c210
Vavou <leptilemurien@hotmail.com>
2016-01-10 18:21:43 +0100

    Update the design of the budget list of the budget screen

--- Flattitude/app/src/main/java/com/aseupc/flattitude/Activities/BudgetActivity.java
+++ Flattitude/app/src/main/java/com/aseupc/flattitude/Activities/BudgetActivity.java
@@ -3,28 +3,29 @@ package com.aseupc.flattitude.Activities;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.Shape;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SeekBar;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
-import com.amulyakhare.textdrawable.TextDrawable;
+import com.aseupc.flattitude.Models.BudgetOperation;
+import com.aseupc.flattitude.Models.Flat;
 import com.aseupc.flattitude.Models.IDs;
+import com.aseupc.flattitude.Models.User;
 import com.aseupc.flattitude.R;
 import com.aseupc.flattitude.utility_REST.ArrayAdapterWithIcon;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BudgetActivity extends AppCompatActivity {
     private Context context;
@@ -51,25 +52,52 @@ public class BudgetActivity extends AppCompatActivity {
         bgPersonalShape.setColor(Color.parseColor("#d0e3e4"));
         personalBalanceView.setText(personalBalance + "€");
 
-        ListView listView = (ListView) findViewById(R.id.listView_budget);
-        listView.setVisibility(View.VISIBLE);
-        ArrayList<String> resultStr = new ArrayList<String>();
-        ArrayList<Integer> imageStr = new ArrayList<Integer>();
-         resultStr.add("Valentin");
-        resultStr.add("Jordi");
-        resultStr.add("Fara");
-        imageStr.add(R.drawable.vavou);
-        imageStr.add(R.drawable.jordi);
-        imageStr.add(R.drawable.fara);
+        Flat currentFlat = IDs.getInstance(context).getFlat(context);
+        User currentUser = IDs.getInstance(context).getUser(context);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                resultStr );
+        // List of fictives operation in order to test
+        ArrayList<BudgetOperation> operations = new ArrayList<BudgetOperation>();
+        operations.add(new BudgetOperation(currentUser, currentFlat, 10, "Putting 10 € on the common account"));
+        operations.add(new BudgetOperation(null, currentFlat, 10, "Shopping for common food"));
+        operations.add(new BudgetOperation(currentUser, currentFlat, 1, "Putting 1 € on the common account"));
 
-        ArrayAdapterWithIcon adapter=new ArrayAdapterWithIcon(this, resultStr, imageStr);
+        List<Map<String, String>> list = new ArrayList<Map<String, String>>();
 
-        listView.setAdapter(adapter);
+        HashMap<String, String> element;
+        for (BudgetOperation operation : operations) {
+            element = new HashMap<String, String>();
+
+            element.put("title", operation.getUser() == null ?
+                    "" + operation.getAmount() + "€ were used from the common budget" :
+                    operation.getUser().getLastname() + " put " + operation.getAmount() + "€ on the common budget");
+            element.put("description", operation.getDescription());
+            list.add(element);
+        }
+
+        ListAdapter adapter = new SimpleAdapter(this, list,
+                android.R.layout.simple_list_item_2,
+                new String[] {"title", "description"},
+                new int[] {android.R.id.text1, android.R.id.text2});
+
+        ListView operationsListView = (ListView) findViewById(R.id.listView_budget);
+        operationsListView.setAdapter(adapter);
+
+//        //operationsListView.setVisibility(View.VISIBLE);
+//        ArrayList<String> resultStr = new ArrayList<String>();
+//        ArrayList<Integer> imageStr = new ArrayList<Integer>();
+//        resultStr.add("Valentin");
+//        resultStr.add("Jordi");
+//        resultStr.add("Fara");
+//        imageStr.add(R.drawable.vavou);
+//        imageStr.add(R.drawable.jordi);
+//        imageStr.add(R.drawable.fara);
+//
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+//                this,
+//                android.R.layout.simple_list_item_1,
+//                resultStr );
+//
+//        ArrayAdapterWithIcon adapter=new ArrayAdapterWithIcon(this, resultStr, imageStr);
     }
 
     @Override
---
+++ Flattitude/app/src/main/java/com/aseupc/flattitude/Models/BudgetOperation.java
@@ -0,0 +1,82 @@
+package com.aseupc.flattitude.Models;
+
+import java.util.Date;
+
+/**
+ * Created by Vavou on 10/01/2016.
+ */
+public class BudgetOperation {
+
+    /**
+     * The user who have done the budget operation
+     * If null, it means that it comes from the common budget of the flat
+     */
+    private User user;
+
+    /**
+     * The flat concerned by the budget operation
+     */
+    private Flat flat;
+
+    /**
+     * The amount of the budget operation
+     */
+    private float amount;
+
+    /**
+     * The description of the budget operation
+     */
+    private String description;
+
+    /**
+     * The date of the budget operation
+     */
+    private Date date;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Flat getFlat() {
+        return flat;
+    }
+
+    public void setFlat(Flat flat) {
+        this.flat = flat;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public BudgetOperation(User user, Flat flat, float amount, String description) {
+        this.user = user;
+        this.flat = flat;
+        this.amount = amount;
+        this.description = description;
+    }
+}
